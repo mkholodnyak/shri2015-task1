1c210a7957b46b2b1cea8ea631023937b3c2fecd
Maxim Kholodnyak <mvkholodnyak@gmail.com>
2015-08-16 08:02:55 +0300

    Обновил gulp-файл

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,50 @@
 var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    rename = require('gulp-rename'),
-    minifyHTML = require('gulp-minify-html'),
-    minifyCss = require('gulp-minify-css'),
+    path = require('path'),
+    $ = require('gulp-load-plugins')({
+        //config: path.join(__dirname, 'package.json'),
+        //lazy: false
+    }),
     browserSync = require('browser-sync').create(),
     liveReload = browserSync.reload;
 
 var config = {
-    outPath: 'public',
-    appPath: 'app',
+    outPath: 'public/',
+    appPath: 'app/',
     htmlSrc: 'index.html'
 };
 
-gulp.task('default', ['start-liveReload-server', 'build-files']);
+gulp.task('default', ['start-livereload-server', 'build-files']);
 
-gulp.task('build-files', ['html', 'css']);
+gulp.task('build-files', ['html', 'css', 'image']);
 
-gulp.task('start-liveReload-server', function () {
+gulp.task('start-livereload-server', function () {
     browserSync.init({
         server: config.outPath
     });
 
     gulp.watch('*.html', ['html']);
+    gulp.watch(config.appPath + '/css/**/*.scss', ['css']);
 });
 
 
 gulp.task('html', function () {
-    gulp.src(config.htmlSrc)
-        .pipe(rename('index.html'))
-        .pipe(minifyHTML())
+    gulp.src('*.html')
+        //.pipe($.rename('index.html'))
+        .pipe($.minifyHtml())
         .pipe(gulp.dest(config.outPath))
         .pipe(liveReload({stream: true}));
 });
 
 gulp.task('css', function () {
-    gulp.src(config.appPath + '/css/**/*.css')
-        .pipe(minifyCss())
-        .pipe(gulp.dest(config.outPath))
+    gulp.src(config.appPath + '/css/**/*.scss')
+        .pipe($.sass().on('error', $.sass.logError))
+        .pipe($.minifyCss())
+        .pipe(gulp.dest(config.outPath + config.appPath + 'css'))
+        .pipe(liveReload({stream: true}));
+});
+
+gulp.task('image', function () {
+    gulp.src(config.appPath + '/images/**/*.{png,svg,jpg,jpeg}')
+        .pipe(gulp.dest(config.outPath + config.appPath + 'images'))
         .pipe(liveReload({stream: true}));
 });
